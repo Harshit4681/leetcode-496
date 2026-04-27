dc76a55f3d5f8dc5c0d519ee6c6c87c23776ecd7
Harshit <sharmaharshit4681@gmail.com>
2026-04-27 22:32:52 +0530

    Add LeetCode solution: Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,25 @@
+﻿public class ClimbingStairs {
+
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(climbStairs(n));
+    }
+}
