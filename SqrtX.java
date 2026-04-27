c657c00e87bdd9e4e708640db7d925f8517a6f12
Harshit <sharmaharshit4681@gmail.com>
2026-04-27 22:32:45 +0530

    Add LeetCode solution: Sqrt(x)

---
+++ SqrtX.java
@@ -0,0 +1,27 @@
+﻿public class SqrtX {
+
+    public static int mySqrt(int x) {
+        long left = 1;
+        long right = x;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (mid * mid == x) {
+                return (int) mid;
+            } else if (mid * mid < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return (int) right;
+    }
+
+    public static void main(String[] args) {
+        int x = 8;
+
+        System.out.println(mySqrt(x));
+    }
+}
