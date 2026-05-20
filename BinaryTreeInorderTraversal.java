d2f87578f34939196a58821f5948841b7f6a9e08
Harshit <sharmaharshit4681@gmail.com>
2026-05-20 23:07:11 +0530

    Add LeetCode solution: Binary Tree Inorder Traversal

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,32 @@
+﻿import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreeInorderTraversal {
+
+    public static void inorder(TreeNode root, List<Integer> result) {
+        if (root == null) return;
+
+        inorder(root.left, result);
+        result.add(root.val);
+        inorder(root.right, result);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+
+        System.out.println(result);
+    }
+}
